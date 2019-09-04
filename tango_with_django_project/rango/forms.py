7d558c7b543b1150f94c93f0d2016baea606d4a9
zhangqi_aliyun <m13281879546@gmail.com>
2019-09-04 22:01:28 +0800

    Add a website '/rango/category/{{ category.slug }}/add_page/'

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -15,11 +15,11 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
 	title = forms.CharField(max_length=128,
-							help_text="please enter the title of the page.")
-			url = forms.URLField(max_length=200, 
-							help_text="please enter the URL of the page.")
-			views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+                help_text="please enter the title of the page.")
+	url = forms.URLField(max_length=200, 
+                help_text="please enter the URL of the page.")
+	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 			
-			class Meta:
-				model = Page
-				exclude = ('category', )
+	class Meta:
+	    model = Page
+	    exclude = ('category', )
